4b4cca49629ef6241c9250c1e227e8064c8d4ee9
Alisha Koirala <12268551@cqumail.com>
2026-05-24 23:24:27 +1000

    Initial commit

---
+++ src/main/java/com/university/disasterresponsesystem/DisasterResponseSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.university.disasterresponsesystem;
+
+/**
+ *
+ * @author vussan
+ */
+public class DisasterResponseSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
