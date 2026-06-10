609c7f476a07efa59550783c8eb41efdebb77533
Alisha Koirala <alisha@bhusan-mac.local>
2026-06-10 12:07:34 +1000

    Created folder structure

---
+++ src/main/java/com/university/disasterresponsesystem/common/model/PriorityLevel.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.university.disasterresponsesystem.common.model;
+
+/**
+ *
+ * @author alisha
+ */
+public class PriorityLevel {
+
+}
---
+++ src/main/java/com/university/disasterresponsesystem/server/DrsServer.java
@@ -0,0 +1,14 @@
+package com.university.disasterresponsesystem.server;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author alisha
+ */
+public class DrsServer {
+
+}
