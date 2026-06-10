c41127cc8e8e94fa3ade280a3b8c23b5ad7a5c2e
Shobiga Jeyasekar <shobiga.jeyasekar@cqumail.com>
2026-06-11 08:43:49 +1000

    coded for AlertController.java

--- src/main/java/com/university/disasterresponsesystem/client/controller/AlertController.java
+++ src/main/java/com/university/disasterresponsesystem/client/controller/AlertController.java
@@ -4,10 +4,65 @@
  */
 package com.university.disasterresponsesystem.client.controller;
 
+import com.university.disasterresponsesystem.client.net.ServerConnection;
+import com.university.disasterresponsesystem.common.model.Alert;
+import com.university.disasterresponsesystem.common.model.AlertSeverity;
+import com.university.disasterresponsesystem.common.protocol.Request;
+import com.university.disasterresponsesystem.common.protocol.RequestType;
+import com.university.disasterresponsesystem.common.protocol.Response;
+import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 /**
  *
  * @author Shobiga Jeyasekar - 12269476
  */
 public class AlertController {
+@FXML private TextField incidentIdField, alertIdField;
+    @FXML private TextArea messageArea;
+    @FXML private ChoiceBox<AlertSeverity> severityChoice;
+    @FXML private ListView<Alert> alertList;
+    @FXML private Label messageLabel;
 
+    @FXML
+    private void initialize() {
+        severityChoice.setItems(FXCollections.observableArrayList(AlertSeverity.values()));
+        refresh();
+    }
+
+    @FXML
+    private void handleCreate() {
+        try {
+            Long incidentId = incidentIdField.getText().isBlank()
+                    ? null : Long.parseLong(incidentIdField.getText().trim());
+            Request req = new Request(RequestType.CREATE_ALERT, ServerConnection.currentUsername())
+                    .put("incidentId", incidentId)
+                    .put("message", messageArea.getText())
+                    .put("severity", severityChoice.getValue().name());
+            messageLabel.setText(ServerConnection.send(req).getMessage());
+            refresh();
+        } catch (NumberFormatException e) {
+            messageLabel.setText("Incident id must be a number (or leave blank)");
+        }
+    }
+
+    @FXML
+    private void handleDeactivate() {
+        try {
+            Request req = new Request(RequestType.DEACTIVATE_ALERT, ServerConnection.currentUsername())
+                    .put("alertId", Long.parseLong(alertIdField.getText().trim()));
+            messageLabel.setText(ServerConnection.send(req).getMessage());
+            refresh();
+        } catch (NumberFormatException e) {
+            messageLabel.setText("Alert id must be a number");
+        }
+    }
+
+    
 }
